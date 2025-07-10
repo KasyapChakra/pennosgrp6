545a27908e2265d3d1e4fb461b3bef585bd51aff
yy04022 <90805303+yy04022@users.noreply.github.com>
2025-07-10 14:09:50 -0400

    fat

--- src/pennfat.c
+++ src/pennfat.c
@@ -8,3 +8,23 @@
 #include "common/pennfat_errors.h"
 #include "internal/pennfat_kernel.h"
 #include "util/utils.h"
+
+// Definition
+
+#define MAX_CMD_LENGTH 1024
+#define BUFSIZE 4096
+
+// function declarations for special routines
+static PennFatErr mkfs(const char* fs_name,
+                       int blocks_in_fat,
+                       int block_size_config);
+static PennFatErr mount(const char* fs_name);
+static PennFatErr unmount();
+static PennFatErr mv(const char* oldname, const char* newname);
+static PennFatErr chmod(const char** args);
+static PennFatErr cp(const char** args);
+
+static void cat(const char** args);
+static void rm(const char** args);
+static void touch(const char** args);
+
